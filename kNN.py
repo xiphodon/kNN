e2f73b8577414c04788b0a6bd86260e773d30e3d
xiphodon <827789895@qq.com>
2016-12-14 11:36:39 +0800

    约会网站预测：归一化数值，测试分类器，建立预测问答入口

--- kNN.py
+++ kNN.py
@@ -9,7 +9,8 @@
 # k-近邻算法
 
 import numpy as np
-import operator # 位运算符模块
+import operator  # 位运算符模块
+
 
 def createDataSet():
     '''
@@ -18,8 +19,9 @@ def createDataSet():
     '''
     # 创建数组，每个元素视为每条数据的两个特征
     group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
-    labels = ['A', 'A', 'B', 'B'] # 数组中每个元素数组对应的标签
+    labels = ['A', 'A', 'B', 'B']  # 数组中每个元素数组对应的标签
     return group, labels
+
 
 def classify0(inX, dataSet, labels, k):
     '''
@@ -30,26 +32,27 @@ def classify0(inX, dataSet, labels, k):
     :param k: 选择最近邻居的数目
     :return: 预测数据所在分类
     '''
-    dataSetSize = dataSet.shape[0] # 获取dataSet矩阵行数，即向量条目数
+    dataSetSize = dataSet.shape[0]  # 获取dataSet矩阵行数，即向量条目数
 
     # 使目标向量形成与dataSet矩阵相同行数矩阵，对应相减
     diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
-    sqDiffMat = diffMat ** 2 # 矩阵各个元素平方
-    sqDistances = sqDiffMat.sum(axis=1) # 一个维度上求和
-    distances = sqDistances ** 0.5 # 开方
+    sqDiffMat = diffMat ** 2  # 矩阵各个元素平方
+    sqDistances = sqDiffMat.sum(axis=1)  # 一个维度上求和
+    distances = sqDistances ** 0.5  # 开方
     # 以上四步为计算目标向量与每个向量间的距离【d=[(x1 - x2)^2 + (y1 - y2)^2]^0.5】
 
-    sortedDistIndicies = distances.argsort() # 距离排序好的索引值
+    sortedDistIndicies = distances.argsort()  # 距离排序好的索引值
     classCount = {}
     # 选取距离最小的k个点
     for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]] # 按距离排好序的每个向量对应的标签值
-        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1 # 统计各个标签出现的次数
+        voteIlabel = labels[sortedDistIndicies[i]]  # 按距离排好序的每个向量对应的标签值
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1  # 统计各个标签出现的次数
 
     # 把字典转换为元组对的列表，按照第2个域值（即标签出现的次数）逆序排序
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     # 返回统计后距离最小的k个点中出现次数最多的标签
     return sortedClassCount[0][0]
+
 
 def kNN_demo1():
     '''
@@ -74,17 +77,17 @@ def file2matrix(filename):
     '''
     fr = open(filename)
     arrayOLines = fr.readlines()
-    numberOfLines = len(arrayOLines) # 获取文件行数
-    returnMat = np.zeros((numberOfLines,3)) # 对应数据转化为矩阵
-    classLabelVector = [] # 类型标签集合
+    numberOfLines = len(arrayOLines)  # 获取文件行数
+    returnMat = np.zeros((numberOfLines, 3))  # 对应数据转化为矩阵
+    classLabelVector = []  # 类型标签集合
     index = 0
     for line in arrayOLines:
         line = line.strip()
         listFromLine = line.split('\t')
-        returnMat[index,:] = listFromLine[0:3]
+        returnMat[index, :] = listFromLine[0:3]
         classLabelVector.append(like_str2int(listFromLine[-1]))
         index += 1
-    return returnMat,classLabelVector
+    return returnMat, classLabelVector
 
 
 def like_str2int(like):
@@ -93,7 +96,9 @@ def like_str2int(like):
     :param like: 魅力值程度字符串
     :return: 魅力值程度对应的数字
     '''
-    if like == 'largeDoses':
+    if like.isdigit():
+        return int(like)
+    elif like == 'largeDoses':
         return 3
     elif like == 'smallDoses':
         return 2
@@ -103,13 +108,84 @@ def like_str2int(like):
         return 0
 
 
+def show_window(datingDataMat, datingLabels):
+    '''
+    使用matplotlib，按标签集来显示不同颜色的“玩视频游戏所耗时间百分比”和“每周所消费的冰淇淋公升数”
+    :param datingDataMat: 数据矩阵
+    :return:
+    '''
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * np.array(datingLabels), 15.0 * np.array(datingLabels))
+    plt.show()
+
+
+def autoNorm(dataSet):
+    '''
+    数据归一化，使所有数据映射在[0,1]区间内
+    {原理：newData = (oldData - min)/(max - min)}
+    :param dataSet: 原数据矩阵
+    :return: 归一化特征值后的矩阵，
+    '''
+    minVals = dataSet.min(0) # 从列中选取最小值 得到1*3行向量
+    maxVals = dataSet.max(0) # 从列中选取最小值 得到1*3行向量
+    ranges = maxVals - minVals
+    normDataSet = np.zeros(np.shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - np.tile(minVals, (m,1)) # oldData - min
+    normDataSet = normDataSet/np.tile(ranges, (m,1))   # (oldData - min)/(max - min)
+    return normDataSet, ranges, minVals
+
+
+def datingClassTest():
+    '''
+    数据分类测试
+    :return:
+    '''
+    hoRatio = 0.10 # 取用10%的数据
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio) # 测试数据选用数量
+    errorCount = 0.0 # 错误计数
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],5)
+        print("划分分类为: %d, 真实分类为: %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print("分类错误率为: %3.2f" % (100*errorCount/float(numTestVecs)) + "%")
+    print("错误数: %d" % (errorCount))
+
+
+def classifyPerson():
+    '''
+    分类约会网友
+    :return: 分类结果
+    '''
+    resultList = [u'不喜欢',u'有点喜欢',u'很喜欢']
+    ffMiles = float(input("每年飞行的里程数:"))
+    percentTats = float(input("玩视频游戏所耗的时间百分比:"))
+    iceCream = float(input("每周消费的冰淇淋公升数:"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = np.array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr - minVals)/ranges, normMat, datingLabels, 3)
+    print("你对这个人的喜欢程度为：", resultList[classifierResult - 1])
+
+
 def kNN_demo2():
     '''
     k-近邻算法改进约会网站的配对效果
     :return:
     '''
-    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
-    print(datingDataMat,datingLabels)
+    # datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    # print(datingDataMat, datingLabels)
+    # show_window(datingDataMat, datingLabels)
+    # normMat,ranges,minVals = autoNorm(datingDataMat)
+    # print(normMat,ranges,minVals)
+    datingClassTest()
+    classifyPerson()
+
 
 if __name__ == '__main__':
     # kNN_demo1()
